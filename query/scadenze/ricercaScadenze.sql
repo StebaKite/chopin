55274d1bb3356de15e167ff5cc71a01f33d6f4e9
stebakite@gmail.com <stebakite@gmail.com>
2015-09-14 22:00:13 +0200

    Aggiunta colonna id_fornitore

--- query/scadenze/ricercaScadenze.sql
+++ query/scadenze/ricercaScadenze.sql
@@ -1,12 +1,15 @@
 SELECT
-	id_scadenza,
-	id_registrazione,
-	to_char(dat_scadenza, 'DD/MM/YYYY') as dat_scadenza,
-	dat_scadenza as dat_scadenza_originale,
-	imp_in_scadenza,
-	nota_scadenza,
-	tip_addebito
+	scadenza.id_scadenza,
+	scadenza.id_registrazione,
+	registrazione.id_fornitore,
+	to_char(scadenza.dat_scadenza, 'DD/MM/YYYY') as dat_scadenza,
+	scadenza.dat_scadenza as dat_scadenza_originale,
+	scadenza.imp_in_scadenza,
+	scadenza.nota_scadenza,
+	scadenza.tip_addebito
 FROM contabilita.scadenza
+	INNER JOIN contabilita.registrazione
+		ON registrazione.id_registrazione = scadenza.id_registrazione
 WHERE 1 = 1
 %filtro_date%
-ORDER BY dat_scadenza_originale
+ORDER BY registrazione.id_fornitore, dat_scadenza_originale
