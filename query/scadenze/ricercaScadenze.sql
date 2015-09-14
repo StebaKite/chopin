d110a5773971976b5f02457b8698dbd40d7ba27b
stebakite@gmail.com <stebakite@gmail.com>
2015-09-14 21:32:18 +0200

    Sistemato controllo quadratura dettagli registrazione

--- query/scadenze/ricercaScadenze.sql
+++ query/scadenze/ricercaScadenze.sql
@@ -2,10 +2,11 @@ SELECT
 	id_scadenza,
 	id_registrazione,
 	to_char(dat_scadenza, 'DD/MM/YYYY') as dat_scadenza,
+	dat_scadenza as dat_scadenza_originale,
 	imp_in_scadenza,
 	nota_scadenza,
 	tip_addebito
 FROM contabilita.scadenza
 WHERE 1 = 1
 %filtro_date%
-ORDER BY dat_scadenza
+ORDER BY dat_scadenza_originale
