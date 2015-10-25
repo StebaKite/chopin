919e7c4b8c4e8a0485d2a7964b9b067c95517ee3
stefano <stefano@stefano-HP-250-G1-Notebook-PC>
2015-10-25 11:45:23 +0100

    Integrate le query con il prelievo del saldo anno precedente dalla tabella SALDO

--- query/riepiloghi/ricavi.sql
+++ query/riepiloghi/ricavi.sql
@@ -1,27 +1,3 @@
-select
-	t3.num_riga_bilancio,
-	t3.ind_visibilita_sottoconti,	
-	t3.des_conto,
-	t1.des_sottoconto,
-	coalesce(sum(t2.imp_registrazione),0) as tot_conto
-  from contabilita.sottoconto as t1
-		inner join contabilita.conto as t3
-			on t3.cod_conto = t1.cod_conto 
-  		left outer join contabilita.dettaglio_registrazione as t2
-  			on  t2.cod_conto = t1.cod_conto
-  	   		and t2.cod_sottoconto = t1.cod_sottoconto
-  		left outer join contabilita.registrazione as t4
-  			on  t4.id_registrazione = t2.id_registrazione
-  where t4.dat_registrazione between '%datareg_da%' and '%datareg_a%'
-  and   t4.cod_negozio = '%codnegozio%'
-  and   t3.cat_conto = '%catconto%'
-  and   t3.ind_presenza_in_bilancio = 'S'  
-  and   t2.ind_dareavere = 'A' 
-group by t3.num_riga_bilancio, t3.ind_visibilita_sottoconti, t3.des_conto, t1.des_sottoconto
-order by t3.num_riga_bilancio, t3.des_conto, t1.des_sottoconto
-
-
-
 SELECT
 	t5.num_riga_bilancio,
 	t5.ind_visibilita_sottoconti,
