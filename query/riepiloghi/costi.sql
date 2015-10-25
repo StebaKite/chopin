8fd0b44b3dc4ce71bf6d80a8453157ec5986ca49
stefano <stefano@stefano-HP-250-G1-Notebook-PC>
2015-10-25 10:39:33 +0100

    Integrate le query del bilancio periodico con il prelievo del riporto saldo dalla tabella SALDO

--- query/riepiloghi/costi.sql
+++ query/riepiloghi/costi.sql
@@ -1,21 +1,50 @@
-select
-	t3.num_riga_bilancio,
-	t3.ind_visibilita_sottoconti,
-	t3.des_conto,
-	t1.des_sottoconto,
-	coalesce(sum(t2.imp_registrazione),0) as tot_conto
-  from contabilita.sottoconto as t1
-		inner join contabilita.conto as t3
-			on t3.cod_conto = t1.cod_conto 
-  		left outer join contabilita.dettaglio_registrazione as t2
-  			on  t2.cod_conto = t1.cod_conto
-  	   		and t2.cod_sottoconto = t1.cod_sottoconto
-  		left outer join contabilita.registrazione as t4
-  			on  t4.id_registrazione = t2.id_registrazione
-  where t4.dat_registrazione between '%datareg_da%' and '%datareg_a%'
-  and   t4.cod_negozio = '%codnegozio%'
-  and   t3.cat_conto = '%catconto%'
-  and   t3.ind_presenza_in_bilancio = 'S'
-  and   t2.ind_dareavere = 'D' 
-group by t3.num_riga_bilancio, t3.ind_visibilita_sottoconti, t3.des_conto, t1.des_sottoconto
-order by t3.num_riga_bilancio, t3.des_conto, t1.des_sottoconto
+SELECT
+	t5.num_riga_bilancio,
+	t5.ind_visibilita_sottoconti,
+	t5.des_conto,
+	t5.des_sottoconto,
+	coalesce(sum(t5.tot_conto),0) as tot_conto
+  FROM (
+		SELECT
+			t3.num_riga_bilancio,
+			t3.ind_visibilita_sottoconti,
+			t3.des_conto,
+			t1.des_sottoconto,
+			coalesce(sum(t2.imp_registrazione),0) as tot_conto
+		  FROM contabilita.sottoconto as t1
+				INNER JOIN contabilita.conto as t3
+					ON t3.cod_conto = t1.cod_conto 
+		  		LEFT OUTER JOIN contabilita.dettaglio_registrazione as t2
+		  			ON  t2.cod_conto = t1.cod_conto
+		  	   		AND t2.cod_sottoconto = t1.cod_sottoconto
+		  		LEFT OUTER JOIN contabilita.registrazione as t4
+		  			ON  t4.id_registrazione = t2.id_registrazione
+		  WHERE t4.dat_registrazione BETWEEN '%datareg_da%' AND '%datareg_a%'
+		  AND   t4.cod_negozio = '%codnegozio%'
+		  AND   t3.cat_conto = '%catconto%'
+		  AND   t3.ind_presenza_in_bilancio = 'S'
+		  AND   t2.ind_dareavere = 'D' 
+		GROUP BY t3.num_riga_bilancio, t3.ind_visibilita_sottoconti, t3.des_conto, t1.des_sottoconto
+
+		UNION
+		
+		SELECT
+			conto.num_riga_bilancio,
+			conto.ind_visibilita_sottoconti,
+			conto.des_conto,
+			sottoconto.des_sottoconto,
+			saldo.imp_saldo as tot_conto
+		 FROM contabilita.saldo  
+			INNER JOIN contabilita.conto as conto
+			  ON conto.cod_conto = saldo.cod_conto
+			INNER JOIN contabilita.sottoconto as sottoconto
+			  ON sottoconto.cod_conto = saldo.cod_conto	  
+			  AND sottoconto.cod_sottoconto = saldo.cod_sottoconto	  
+		 WHERE saldo.dat_saldo = '%datareg_da%'
+		  AND saldo.cod_negozio = '%codnegozio%'		  
+		  AND saldo.ind_dareavere = 'D' 		  
+		  AND conto.cat_conto = '%catconto%'
+		  AND conto.ind_presenza_in_bilancio = 'S'
+	) t5 
+GROUP BY t5.num_riga_bilancio, t5.ind_visibilita_sottoconti, t5.des_conto, t5.des_sottoconto	
+ORDER BY t5.num_riga_bilancio, t5.des_conto, t5.des_sottoconto
