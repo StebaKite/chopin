9ac8020b6d09f170f6a8b670648766f6162f81a4
stefano <stefano@stefano-HP-250-G1-Notebook-PC>
2016-02-02 21:24:21 +0100

    Tolte le query per la generazione dei grafici e della tabella eventi

--- src/main/corpo.class.php
+++ src/main/corpo.class.php
@@ -5,11 +5,6 @@ require_once 'chopin.abstract.class.php';
 class Corpo extends ChopinAbstract {
 
 	public static $messaggio;
-	public static $queryEventi = "/main/eventi.sql";
-	public static $queryTrendPagamenti = "/riepiloghi/trendPagamenti.sql";
-	public static $queryTrendIncassi = "/riepiloghi/trendIncassi.sql";
-	public static $queryMesi = "/riepiloghi/mesi.sql";
-
 	private static $_instance = null;
 
 	function __construct() {
@@ -47,130 +42,10 @@ class Corpo extends ChopinAbstract {
 	public function start() {
 
 		require_once 'corpo.template.php';
-		require_once 'utility.class.php';
-		require_once 'database.class.php';
 		
 		$corpoTemplate = CorpoTemplate::getInstance();
-		
-		$utility = Utility::getInstance();
-		$db = Database::getInstance();
-		
-		$array = $utility->getConfig();
-
-		$testata = self::$root . $array['testataPagina'];
-		$piede = self::$root . $array['piedePagina'];
-
-		//- Box degli eventi ------------------------------------------------------------
-
-		$filtroEventi = "";
-		
-		if ($_SESSION["statoeventi"] != "") {
-			$filtroEventi = "WHERE sta_evento = '" . $_SESSION["statoeventi"] . "'"; 			
-		} 
-		
-		$replace = array(
-				'%filtro_eventi%' => $filtroEventi
-		);
-		
-		$array = $utility->getConfig();
-		$sqlTemplate = self::$root . $array['query'] . self::$queryEventi;
-		
-		$sql = $utility->tailFile($utility->getTemplate($sqlTemplate), $replace);
-		$result = $db->getData($sql);
-	
-		if ($result) $_SESSION["eventi"] = pg_fetch_all($result);
-		else $_SESSION["eventi"] = "";
-
-		//- Box trends ------------------------------------------------------------
-
-		if ($array['primoSaldoDisponibile'] != "") { $_SESSION["datareg_da"] = $array['primoSaldoDisponibile']; }
-		else { $_SESSION["datareg_da"] = "01/01/" . date("Y"); }
-		
-		if ($array['ultimoSaldoDisponibile'] != "") { $_SESSION["datareg_a"] = $array['ultimoSaldoDisponibile']; }
-		else { $_SESSION["datareg_a"] = "31/12/" . date("Y"); }
-		
-		$this->ricercaPagamenti($utility, $db, "VIL");
-		$this->ricercaPagamenti($utility, $db, "BRE");
-		$this->ricercaPagamenti($utility, $db, "TRE");		
-
-		$this->ricercaIncassi($utility, $db, "VIL");
-		$this->ricercaIncassi($utility, $db, "BRE");
-		$this->ricercaIncassi($utility, $db, "TRE");
-		
-		$this->ricercaMesi($utility, $db);
-				
-		// compone la pagina
-//		include($testata);
 		$corpoTemplate->displayPagina();
-//		include($piede);
-	}	
-
-	public function ricercaPagamenti($utility, $db, $negozio) {
-	
-		$array = $utility->getConfig();
-	
-		$replace = array(
-				'%datareg_da%' => $_SESSION["datareg_da"],
-				'%datareg_a%' => $_SESSION["datareg_a"],
-				'%codnegozio%' => $negozio
-		);
-	
-		$sqlTemplate = self::$root . $array['query'] . self::$queryTrendPagamenti;
-		$sql = $utility->tailFile($utility->getTemplate($sqlTemplate), $replace);
-		$result = $db->getData($sql);
-	
-		$dati = "trendPagamenti" . $negozio;
-		
-		if (pg_num_rows($result) > 0) {
-			$_SESSION[$dati] = $result;
-		}
-		else {
-			unset($_SESSION[$dati]);
-		}
-		return $result;
 	}
-
-	public function ricercaIncassi($utility, $db, $negozio) {
-	
-		$array = $utility->getConfig();
-	
-		$replace = array(
-				'%datareg_da%' => $_SESSION["datareg_da"],
-				'%datareg_a%' => $_SESSION["datareg_a"],
-				'%codnegozio%' => $negozio
-		);
-	
-		$sqlTemplate = self::$root . $array['query'] . self::$queryTrendIncassi;
-		$sql = $utility->tailFile($utility->getTemplate($sqlTemplate), $replace);
-		$result = $db->getData($sql);
-	
-		$dati = "trendIncassi" . $negozio;
-	
-		if (pg_num_rows($result) > 0) {
-			$_SESSION[$dati] = $result;
-		}
-		else {
-			unset($_SESSION[$dati]);
-		}
-		return $result;
-	}
-	
-	public function ricercaMesi($utility, $db) {
-	
-		$array = $utility->getConfig();
-	
-		$sqlTemplate = self::$root . $array['query'] . self::$queryMesi;
-		$sql = $utility->getTemplate($sqlTemplate);
-		$result = $db->getData($sql);
-	
-		if (pg_num_rows($result) > 0) {
-			$_SESSION["mesi"] = $result;
-		}
-		else {
-			unset($_SESSION["mesi"]);
-		}
-		return $result;
-	}	
 }
 
 ?>
