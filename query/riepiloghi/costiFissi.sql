b1f94e8f3754f22434e6a347425a7c8ff3c16686
Stefano <stebakite@gmail.com>
2016-02-14 17:53:35 +0100

    sistemazione query x estrazione numeri per calcolo MCT e BEP

--- query/riepiloghi/costiFissi.sql
+++ query/riepiloghi/costiFissi.sql
@@ -1,5 +1,31 @@
-select sum(imp_saldo) as totalecostofisso
-  from contabilita.saldo
- where cod_negozio IN (%codnegozio%)
-   and cod_conto not in ('350','360')
-   and dat_saldo = '%datareg_da%'
+SELECT
+	COALESCE(sum(t4.tot_conto * t4.ind_dareavere),0) as totalecostofisso
+  FROM (
+  		SELECT 
+				CASE 
+					WHEN t2.ind_dareavere = 'D' then  1
+					WHEN t2.ind_dareavere = 'A' then -1
+				END AS ind_dareavere,	
+				SUM(t2.imp_registrazione) as tot_conto		  
+				
+ 		FROM contabilita.sottoconto as t1
+ 		
+			INNER JOIN contabilita.conto as t3
+				ON t3.cod_conto = t1.cod_conto 
+				
+	  		LEFT OUTER JOIN contabilita.dettaglio_registrazione as t2
+	  			ON  t2.cod_conto = t1.cod_conto
+	  	   		AND t2.cod_sottoconto = t1.cod_sottoconto
+	  	   		
+	  		LEFT OUTER JOIN contabilita.registrazione as t4
+	  			ON  t4.id_registrazione = t2.id_registrazione
+	  			
+	  WHERE t4.dat_registrazione BETWEEN '%datareg_da%' AND '%datareg_a%'
+	  AND   t4.cod_negozio IN (%codnegozio%)
+	  AND   t3.cat_conto = 'Conto Economico'
+	  AND   t3.cod_conto not in ('300')
+	  AND   t3.tip_conto = 'Dare'
+	  
+	GROUP BY t2.ind_dareavere
+) AS t4	
+   
