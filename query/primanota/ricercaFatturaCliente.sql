a643db8a61308fcc34b9d2c0fb48770d9a015819
Stefano <stebakite@gmail.com>
2016-03-20 12:15:11 +0100

    Fix : la ricerca delle fatture deve essere fatta all'interno delle fatture dell'anno in corso

--- query/primanota/ricercaFatturaCliente.sql
+++ query/primanota/ricercaFatturaCliente.sql
@@ -5,3 +5,4 @@ SELECT
 FROM contabilita.registrazione
 WHERE id_cliente = %id_cliente%
 AND   num_fattura = '%num_fattura%'
+AND   extract(year from dat_registrazione) = extract(year from current_date)
--- query/primanota/ricercaFatturaFornitore.sql
+++ query/primanota/ricercaFatturaFornitore.sql
@@ -5,3 +5,4 @@ SELECT
 FROM contabilita.registrazione
 WHERE id_fornitore = %id_fornitore%
 AND   num_fattura = '%num_fattura%'
+AND   extract(year from dat_registrazione) = extract(year from current_date)
