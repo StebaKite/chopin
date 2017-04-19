1f054be39cf3d895d27181ece13373d7c241ce0b
stefano <stebakite@gmail.com>
2017-04-19 19:39:42 +0200

    font e allineamenti fatture vendita e contributo cliente

--- src/fatture/fatturaCliente.class.php
+++ src/fatture/fatturaCliente.class.php
@@ -22,45 +22,45 @@ class FatturaCliente extends Fattura {
 	}
 
 	public function identificativiFatturaCliente($giorno, $meserif, $anno, $numfat, $codneg) {
-	
+
 		$negozio = ($codneg == "VIL") ? "Villa d'Adda" : $negozio;
 		$negozio = ($codneg == "TRE") ? "Trezzo" : $negozio;
 		$negozio = ($codneg == "BRE") ? "Brembate" : $negozio;
-	
+
 		/**
-		 * Eccezione: 
+		 * Eccezione:
 		 * - le fatture i Brembate hanno una B dopo il progressivo
 		 * - le fatture di Trezzo hanno una T dopo il progressivo
 		 */
-		
+
 		$nfat = str_pad($numfat, 2, "0", STR_PAD_LEFT);
 		$nfat = ($codneg == "BRE") ? str_pad($numfat, 2, "0", STR_PAD_LEFT) . "B" : $nfat;
 		$nfat = ($codneg == "TRE") ? str_pad($numfat, 2, "0", STR_PAD_LEFT) . "T" : $nfat;
-		
+
 		$r1  = 10;
 		$r2  = $r1 + 192;
 		$y1  = 94;
 		$y2  = $y1+10;
-		$mid = $y1 + (($y2-$y1) / 2); 
+		$mid = $y1 + (($y2-$y1) / 2);
 		$this->SetFillColor(189, 229, 244);
 		$this->RoundedRect($r1, $y1, ($r2 - $r1), ($y2-$y1), 2.5, 'DF');
 		$this->SetXY( $r1 + 5, $y1 + 3 );
 		$this->SetFont( "Arial", "B", 10);
-		$this->Cell(10,4, "REG. SEZ. 0" . str_repeat(" ",48) . $negozio . "     Fattura N. :  " . $nfat . "/" . $anno . "   del  " . $giorno . " " . $meserif . " " . $anno, 0, 0, "");
+		$this->Cell(10,4, "REG. SEZ. 0" . str_repeat(" ",53) . $negozio . "     Fattura N. :  " . $nfat . "/" . $anno . "   del  " . $giorno . " " . $meserif . " " . $anno, 0, 0, "");
 	}
 
 	public function aggiungiLineaLiberaCliente($w, $linea, $r1, $y1) {
-	
+
 		$this->SetXY( $r1, $y1 );
 		$this->SetFont( "Arial", "", 12);
-	
+
 		$articolo = explode("\\", $linea["ARTICOLO"]);
-	
+
 		$this->Cell($w[0],6,iconv('UTF-8', 'windows-1252', $articolo[0]),"");
 		$this->Cell($w[1],6,EURO,"",0,'R');
 		$this->Cell($w[2],6,number_format($linea["TOTALE"], 2, ',', '.'),"",0,'R');
 		$this->Ln();
-	
+
 		for($i=1;$i<count($articolo);$i++) {
 			$this->Cell($w[1],6,iconv('UTF-8', 'windows-1252', $articolo[$i]),"",0,"L");
 			$this->Ln();
@@ -68,7 +68,7 @@ class FatturaCliente extends Fattura {
 	}
 
 // 	public function totaliFatturaContributoCliente($tot_dettagli, $tot_imponibile, $tot_iva) {
-	
+
 // 		$this->SetFont( "Arial", "B", 10);
 // 		$r1  = 10;
 // 		$r2  = $r1 + 192;
@@ -79,14 +79,14 @@ class FatturaCliente extends Fattura {
 // 		$this->Line( $r1, $y1+6, $r2, $y1+6);
 // 		$this->Line( $r1+50, $y1, $r1+50, $y2);  // davanti all' IVA
 // 		$this->Line( $r1+100, $y1, $r1+100, $y2);  // davanti al totale
-	
+
 // 		$this->SetXY( $r1, $y1);
 // 		$this->Cell(50,6, "IMPONIBILE","",0,"R");
 // 		$this->SetX( $r1+50 );
 // 		$this->Cell(50,6, "IVA 4%","",0,"R");
 // 		$this->SetX( $r1+140 );
 // 		$this->Cell(50,6, "NETTO A PAGARE","",0,"R");
-	
+
 // 		$this->SetXY( $r1, $y1+7);
 // 		$this->Cell(50,6,number_format($tot_imponibile, 2, ',', '.'),"",0,"R");
 // 		$this->SetXY( $r1+50, $y1+7);
@@ -110,9 +110,9 @@ class FatturaCliente extends Fattura {
 		$this->Line( $r1+15, $y1, $r1+15, $y2);  // davanti alla descrizione
 		$this->Line( $r1+50, $y1, $r1+50, $y2);  // davanti all'imponibile
 		$this->Line( $r1+75, $y1, $r1+75, $y2);  // davanti all'imposta
-		
+
 		$this->SetFont( "Arial", "B", 10);
-		
+
 		$this->SetXY( $r1+2, $y1);
 		$this->Cell(10,6, "IVA");
 		$this->SetX( $r1+20 );
@@ -121,9 +121,9 @@ class FatturaCliente extends Fattura {
 		$this->Cell(10,6, "IMPONIBILE");
 		$this->SetX( $r1+80 );
 		$this->Cell(10,6, "IMPOSTA");
-		
-		$this->SetFont( "Arial", "", 10);
-		
+
+		$this->SetFont( "Arial", "", 12);
+
 		if ($tot_imponibile > 0) {
 			$this->SetXY( $r1+2, $y1+7);
 			$this->Cell(10,6,$aliquota_iva . "%","",0,"C");
@@ -146,25 +146,25 @@ class FatturaCliente extends Fattura {
 		$this->RoundedRect($r1, $y1, ($r2 - $r1), ($y2-$y1), 2.5, 'DF');
 		$mid = $y1 + (($y2-$y1) / 2);
 		$this->Line( $r1, $mid, $r2, $mid);
-		
+
 		$this->SetFont( "Arial", "B", 10);
-		
+
 		$this->SetXY( $r1+28, $y1);
 		$this->Cell(10,6, "Tot. Imponibile","",0,"R");
 		$this->SetXY( $r1+28, $y1+12);
 		$this->Cell(10,6, "Tot. Imposta","",0,"R");
-		
+
 		$imponibile = $tot_imponibile;
 		$iva = $tot_iva;
-		
-		$this->SetFont( "Arial", "", 10);
-		
+
+		$this->SetFont( "Arial", "", 12);
+
 		$this->SetXY( $r1+28, $y1+6);
 		$this->Cell(10,6,number_format($imponibile, 2, ',', '.'),"",0,"R");
-		
+
 		$this->SetXY( $r1+28, $y1+18);
 		$this->Cell(10,6,number_format($iva, 2, ',', '.'),"",0,"R");
-		
+
 		/**
 		 * Box totale documento
 		*/
@@ -174,22 +174,22 @@ class FatturaCliente extends Fattura {
 		$y2  = $y1+25;
 		$this->SetFillColor(230, 230, 230);
 		$this->RoundedRect($r1, $y1, ($r2 - $r1), ($y2-$y1), 2.5, 'DF');
-		
+
 		$this->SetFont( "Arial", "B", 10);
-		
+
 		$this->SetXY( $r1+34, $y1);
 		$this->Cell(10,6, "TOTALE","",0,"R");
-		
+
 		$netto = $imponibile + $iva;
-		
+
 		$this->SetFont( "Arial", "B", 14);
-		
+
 		$this->SetXY( $r1+34, $y1+10);
 		$this->Cell(10,6,EURO . " " . number_format($netto, 2, ',', '.'),"",0,"R");
 	}
-	
+
 	public function totaliFatturaVenditaCliente($tot_imponibile, $tot_iva, $tot_imponibile_10, $tot_iva_10, $tot_imponibile_22, $tot_iva_22) {
-	
+
 		/**
 		 * Box riepilogo imponibili e aliquote IVA
 		 */
@@ -203,9 +203,9 @@ class FatturaCliente extends Fattura {
 		$this->Line( $r1+15, $y1, $r1+15, $y2);  // davanti alla descrizione
 		$this->Line( $r1+50, $y1, $r1+50, $y2);  // davanti all'imponibile
 		$this->Line( $r1+75, $y1, $r1+75, $y2);  // davanti all'imposta
-	
+
 		$this->SetFont( "Arial", "B", 10);
-	
+
 		$this->SetXY( $r1+2, $y1);
 		$this->Cell(10,6, "C.IVA");
 		$this->SetX( $r1+20 );
@@ -214,8 +214,8 @@ class FatturaCliente extends Fattura {
 		$this->Cell(10,6, "IMPONIBILE");
 		$this->SetX( $r1+80 );
 		$this->Cell(10,6, "IMPOSTA");
-	
-		$this->SetFont( "Arial", "", 10);
+
+		$this->SetFont( "Arial", "", 12);
 
 		if ($tot_imponibile > 0) {
 			$_y1 = $y1 + 7;
@@ -228,7 +228,7 @@ class FatturaCliente extends Fattura {
 			$this->SetX( $r1+80 );
 			$this->Cell(18,6,number_format($tot_iva, 2, ',', '.'),"",0,"R");
 		}
-		
+
 		if ($tot_imponibile_10 > 0) {
 			$_y1 = $y1 + 12;
 			$this->SetXY( $r1+2, $_y1);
@@ -240,7 +240,7 @@ class FatturaCliente extends Fattura {
 			$this->SetX( $r1+80 );
 			$this->Cell(18,6,number_format($tot_iva_10, 2, ',', '.'),"",0,"R");
 		}
-	
+
 		if ($tot_imponibile_22 > 0) {
 			$_y1 = $y1 + 17;
 			$this->SetXY( $r1+2, $_y1);
@@ -252,7 +252,7 @@ class FatturaCliente extends Fattura {
 			$this->SetX( $r1+80 );
 			$this->Cell(18,6,number_format($tot_iva_22, 2, ',', '.'),"",0,"R");
 		}
-	
+
 		/**
 		 * Box riepilogo Totale imponibile e iva
 		 */
@@ -264,25 +264,25 @@ class FatturaCliente extends Fattura {
 		$this->RoundedRect($r1, $y1, ($r2 - $r1), ($y2-$y1), 2.5, 'DF');
 		$mid = $y1 + (($y2-$y1) / 2);
 		$this->Line( $r1, $mid, $r2, $mid);
-	
+
 		$this->SetFont( "Arial", "B", 10);
-	
+
 		$this->SetXY( $r1+28, $y1);
 		$this->Cell(10,6, "Tot. Imponibile","",0,"R");
 		$this->SetXY( $r1+28, $y1+12);
 		$this->Cell(10,6, "Tot. Imposta","",0,"R");
-	
+
 		$imponibile = $tot_imponibile + $tot_imponibile_10 + $tot_imponibile_22;
 		$iva = $tot_iva + $tot_iva_10 + $tot_iva_22;
-	
-		$this->SetFont( "Arial", "", 10);
-	
+
+		$this->SetFont( "Arial", "", 12);
+
 		$this->SetXY( $r1+28, $y1+6);
 		$this->Cell(10,6,number_format($imponibile, 2, ',', '.'),"",0,"R");
-	
+
 		$this->SetXY( $r1+28, $y1+18);
 		$this->Cell(10,6,number_format($iva, 2, ',', '.'),"",0,"R");
-	
+
 		/**
 		 * Box totale documento
 		*/
@@ -292,16 +292,16 @@ class FatturaCliente extends Fattura {
 		$y2  = $y1+25;
 		$this->SetFillColor(230, 230, 230);
 		$this->RoundedRect($r1, $y1, ($r2 - $r1), ($y2-$y1), 2.5, 'DF');
-	
+
 		$this->SetFont( "Arial", "B", 10);
-	
+
 		$this->SetXY( $r1+34, $y1);
 		$this->Cell(10,6, "TOTALE","",0,"R");
-	
+
 		$netto = $imponibile + $iva;
-	
+
 		$this->SetFont( "Arial", "B", 14);
-	
+
 		$this->SetXY( $r1+34, $y1+10);
 		$this->Cell(10,6,EURO . " " . number_format($netto, 2, ',', '.'),"",0,"R");
 	}
